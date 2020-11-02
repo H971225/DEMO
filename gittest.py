6d961b58efbc6a1cbe032fe8b02db92c14fee5b6
tpoaax <496519396@qq.com>
2020-11-02 08:27:27 +0800

    First commit~

---
+++ gittest.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"hi, I am David, nice to meet you! I'm {age} old, {height} tall and {weight} heavy.")
